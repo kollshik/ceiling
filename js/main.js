d7c0d4114c92f09d8cbaa90db87523337370dbda
Иван Киреев <108953652+kollshik@users.noreply.github.com>
2024-03-13 22:14:16 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  $('.header__slider__wrapper').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    centerMode: true,
+    easing: "linear",
+    pauseOnFocus:false,
+    
+    
+  })
+
+})
